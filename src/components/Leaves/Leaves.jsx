5673a77b2e178d28439d6d26cba8eed849a68401
Melanie <melaniehernandez153@hotmail.com>
2025-01-14 11:40:37 -0400

    proyect branch draw added, leaves done

--- src/components/Leaves/Leaves.jsx
+++ src/components/Leaves/Leaves.jsx
@@ -1,5 +1,17 @@
 import "./Leaves.css";
 
-export const Leaves = () => {
-  return <div className="upLeaves"></div>;
+export const Leaves = ({ TypeLeaves }) => {
+  return (
+    <div className={TypeLeaves}>
+      <div className="leaveContainer">
+        <div className="upLeave frac"></div>
+        <div className="downLeave frac">
+          <div className="textContainer">
+            <p className="proyectTitle">Pet Myproductivity</p>
+            <p> A minigame app about pets and produtivity</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/Proyects/Proyects.jsx
+++ src/pages/Proyects/Proyects.jsx
@@ -8,24 +8,27 @@ export const Proyects = () => {
         <h1>Proyects</h1>
       </div>
 
-      <div className="branch">
-        <h2>websites</h2>
-        <div className="leaves">
-          <Leaves />
-          <Leaves />
-          <Leaves />
-          <Leaves />
-        </div>
-        <div className="trunk">
-          <img src="" alt="here goes an image" />
-        </div>
+      <div className="branchContainer">
+        <div className="branch">
+          <h2>websites</h2>
+          <div className="leaves">
+            <Leaves TypeLeaves={"upLeaves"} />
+            <Leaves TypeLeaves={"upLeaves"} />
+            <Leaves TypeLeaves={"upLeaves"} />
+            <Leaves TypeLeaves={"upLeaves"} />
+          </div>
+          <div className="trunk">
+            <div className="trunkhole"></div>
+          </div>
 
-        <div className="leaves">
-          <Leaves />
-          <Leaves />
+          <div className="leaves">
+            <Leaves TypeLeaves={"downLeaves"} />
+            <Leaves TypeLeaves={"downLeaves"} />
+          </div>
+          <h2>mini-games & mods</h2>
         </div>
-        <h2>mini-games & mods</h2>
       </div>
+
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia est aperiam dolores, quos omnis nostrum magnam iure adipisci nulla, praesentium suscipit temporibus error atque
         necessitatibus voluptatum soluta neque nam dolorem.
