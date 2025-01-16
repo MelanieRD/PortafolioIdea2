b28b8845ac316e9163a97e835bda7c6bb09c8a4c
Melanie <melaniehernandez153@hotmail.com>
2025-01-16 11:41:40 -0400

    --allow-emptyflag

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,38 @@
+import { useInView, animated, useSpring } from "@react-spring/web";
 import { ContentAbout } from "../../components/ContentAbout/ContentAbout";
 import { ImgCenterHome } from "../../components/ImgCenterHome/ImgCenterHome";
 import { OpacityDiv } from "../../components/OpacityDiv/OpacityDiv";
 import "./home.css";
+import { useEffect } from "react";
 
 export const Home = () => {
+
+  const [ Aboutref, AboutinView] = useInView({
+    //top right bottom left
+    threshold: 0.60, 
+  });
+
+  const [exitTransition, exitTransitionAPI] = useSpring(()=>({
+    from: { opacity: 0,  },
+    to: { opacity: 1 },
+    config: { duration: 1000 },
+  }));
+
+  useEffect(() => {
+    console.log("inview ABOUT"+AboutinView);
+    if (AboutinView) {
+      exitTransitionAPI.start({ opacity: 1 });
+    } else {
+      exitTransitionAPI.start({ opacity: 0 });
+    }
+  }, [AboutinView]);
+
+  
+
+
   return (
-    <OpacityDiv durationSec={1000} opacityFrom={0} opacityTo={1}>
-      <div className="home">
+   
+      <animated.div className="home"ref={Aboutref} style={exitTransition}>
         <div className="content">
           <ContentAbout />
         </div>
@@ -15,7 +41,7 @@ export const Home = () => {
             <ImgCenterHome />
           </div>
         </div>
-      </div>
-    </OpacityDiv>
+      </animated.div>
+    
   );
 };
