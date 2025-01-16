73bee3bb8110a99af9a29df18dad4d38b5f45645
Melanie <melaniehernandez153@hotmail.com>
2025-01-15 23:13:29 -0400

    --allow-emptyflag

--- src/components/ImgCenterHome/ImgCenterHome.jsx
+++ src/components/ImgCenterHome/ImgCenterHome.jsx
@@ -1,11 +1,15 @@
 import "./imgCenterHome.css";
 
 export const ImgCenterHome = () => {
+  //   el imgContainer  estaba envuelto en un div conc lase dropdesign<div className="dropdesign">
+
   return (
-    <div className="dropdesign">
+    
+    
       <div className="imgContainer">
+        
         <img className="imgCharacter" src="/img/girl2.png" alt="" />
-      </div>
+    
     </div>
   );
 };
