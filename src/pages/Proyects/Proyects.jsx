b654fc05a1a1a068734a9b2a6afd8618637d2509
Melanie <melaniehernandez153@hotmail.com>
2025-01-17 16:09:52 -0400

    --allow-emptyflag

--- src/pages/Proyects/Proyects.jsx
+++ src/pages/Proyects/Proyects.jsx
@@ -1,23 +1,28 @@
 import { to, useTrail, animated, useSpring, useInView } from "@react-spring/web";
 import { Leaves } from "../../components/Leaves/Leaves";
 import "./proyects.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Proyects = () => {
   const titleText = "Proyects".split("");
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); 
   const [ref, inView] = useInView({
     //top right bottom left
     threshold: 0.7, // El 50% del elemento debe estar visible para que se considere "en vista"
   });
   
   useEffect(() => {
+    let timeAnimationDuration = 500; // Duración de la animación en milisegundos
+
+    if(isMobile){timeAnimationDuration = 1000;} // Si es mobile, la duración de la animación es de 500ms
+
     console.log(inView);
     if (inView) {
       proyectsTextTrailAPI.start({ opacity: 1 });
       lefttoRightAPI.start({x: 0});
     } else {
       proyectsTextTrailAPI.start({ opacity: 0 });
-      lefttoRightAPI.start({x: -1000, config: { duration: 500 }});
+      lefttoRightAPI.start({x: -1000, config: { duration: timeAnimationDuration }});
     }
   }, [inView]);
 
@@ -34,7 +39,7 @@ export const Proyects = () => {
   const [lefttoRight, lefttoRightAPI] = useSpring(() => ({
     from: { x: 0  },
     to: { x: -1000  },
-    config: { duration: 100 },
+    config: { duration: 500 },
   }));
 
   return (
