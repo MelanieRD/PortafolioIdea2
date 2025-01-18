a4a562d37d7123621045374da4fefcba358eccf6
Melanie <melaniehernandez153@hotmail.com>
2025-01-17 20:06:34 -0400

    --allow-emptyflag

--- src/components/BranchDecoration/FlowerCornerDeco/FlowerCornerDeco.jsx
+++ src/components/BranchDecoration/FlowerCornerDeco/FlowerCornerDeco.jsx
@@ -5,9 +5,10 @@ import { LeavesDeco } from "../LeavesDeco/LeavesDeco";
 
     return (
         <>
-          <LeavesDeco TypeLeaves={"cornerRightLDeco"} Rotationnum={10} translateXnum={-10} translateYnum={5} scale={1.3} Color1={"rgb(252,209,215)"} Color2={"rgb(233,177,205)"} Color1Opacity={"rgba(253, 158, 225, 0.222)"}/>
-          <LeavesDeco TypeLeaves={"cornerRightLDeco"} Rotationnum={55} translateXnum={-3} translateYnum={5} scale={1.3} Color1={"rgb(252,209,215)"} Color2={"rgb(233,177,205)"} Color1Opacity={"rgba(253, 158, 225, 0.222)"} />
-           <LeavesDeco TypeLeaves={"cornerRightLDeco"} Rotationnum={80} translateXnum={-10} translateYnum={10} scale={1.4} Color1={"rgb(252,209,215)"} Color2={"rgb(233,177,205)"} Color1Opacity={"rgba(253, 158, 225, 0.222)"}/>
+        {/* <LeavesDeco TypeLeaves={"cornerRightLDeco"} Rotationnum={10} translateXnum={-10} translateYnum={5} scale={1.3} Color1={"rgb(252,209,215)"} Color2={"rgb(233,177,205)"} Color1Opacity={"rgba(253, 158, 225, 0.222)"}/> */}
+          <LeavesDeco TypeLeaves={"cornerRightLDeco"} Rotationnum={10} translateXnum={-10} translateYnum={5} scale={1.3} Color1={"rgb(236,123,164)"} Color2={"rgb(240, 78, 137)"} Color1Opacity={"rgba(253, 158, 225, 0.14)"}/>
+          <LeavesDeco TypeLeaves={"cornerRightLDeco"} Rotationnum={55} translateXnum={-3} translateYnum={5} scale={1.3} Color1={"rgb(236,123,164)"} Color2={"rgb(240, 78, 137)"} Color1Opacity={"rgba(253, 158, 225, 0.05)"} />
+           <LeavesDeco TypeLeaves={"cornerRightLDeco"} Rotationnum={80} translateXnum={-10} translateYnum={10} scale={1.4} Color1={"rgb(236,123,164)"} Color2={"rgb(240, 78, 137)"} Color1Opacity={"rgba(253, 158, 225, 0.222)"}/>
         </>
     );
  }
