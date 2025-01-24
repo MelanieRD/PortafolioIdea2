b98f65b7c5f37d5ec250a2cb605de952ec89be78
Melanie <melaniehernandez153@hotmail.com>
2025-01-24 12:04:20 -0400

    --allow-emptyflag

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import { animated, useTrail } from "@react-spring/web";
 import { FaArtstation } from "react-icons/fa";
 import { FlowerCornerDeco } from "../../components/BranchDecoration/FlowerCornerDeco/FlowerCornerDeco";
 import { TechIcon } from "../../components/TechIcon/TechIcon";
+import { PiGithubLogo } from "react-icons/pi";
 export const Contact = () => {
     const imgUrl = "url(./img/";
     const extension = ".png)";
@@ -22,30 +23,40 @@ export const Contact = () => {
         []
       ); 
 
+      const handleLinkdinClick = () => {
+        window.open("https://www.linkedin.com/in/melanimelendez/", "_blank");
+        console.log("linkedin");
+      }
+      const handleGithubClick = () => {
+        window.open("https://github.com/MelanieRD", "_blank");
+        console.log("github");
+      }
+
     return (
         <div className="contactContainer">
 
                 
                 <div className="upContactContainer">
-                <div className="sectionLeft">
-                    <div className=" cornerDecor" style={{ transform: "scaleY(-1)" }}>
-                    <FlowerCornerDeco/>
-                        
-                        {/* <LeavesDeco TypeLeaves={"cornerRightLDeco"} Rotationnum={0} translateXnum={0} translateYnum={0}  /> */}
-                    </div>
-                </div>
-                <div className="titleContact"> <h2>Get in touch</h2></div>
+                  <div className="sectionLeft">
+                      <div className=" cornerDecor" style={{ transform: "scaleY(-1)" }}>
+                        <FlowerCornerDeco/>
+                      </div>
+                  </div>
 
-                <div className="sectionRight">
-                <div className=" cornerDecor" style={{transform: "rotate(180deg)"}}>
-                        <FlowerCornerDeco/>        
-                 </div>    
-                </div>
+                  <div className="titleContact"> <h2>Get in touch</h2></div>
+
+                  <div className="sectionRight">
+                    <div className=" cornerDecor" style={{transform: "rotate(180deg)"}}>
+                      <FlowerCornerDeco/>        
+                    </div>    
+                  </div>
                 </div>
 
-            <div className="middleContactContainer">
-              <TechIcon iconTag={<LiaLinkedin className="icon"/>}/>
+              <div className="middleContactContainer">
+                  <TechIcon iconTag={<LiaLinkedin className="iconRow"/>} imgBackground={"url(/img/container1.png)"}   paddingVW={"0.2vw"} addclassName={"iconRowContainer"} eventOnclick={()=>{handleLinkdinClick()}}/>
+                  <TechIcon iconTag={<PiGithubLogo className="iconRow"/>} imgBackground={"url(/img/container1.png)"}   paddingVW={"0.2vw"} addclassName={"iconRowContainer"} eventOnclick={()=>{handleGithubClick()}} />   
             </div>
+
             <div className="wave"></div>
             <div className="downContactContainer">
 
